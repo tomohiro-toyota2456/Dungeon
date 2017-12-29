ce62a9282c93db2d7149feb3738ec0349b4e984b
豊田智弘 <toyota@192.168.2.116>
2017-12-30 01:03:29 +0900

    SceneChange　テスト

---
+++ Assets/Common/Script/SceneManager/ChangeScene.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+	private string preview;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	IEnumerator LoadScene(string loadSceneName)
+	{
+		if(!string.IsNullOrEmpty(preview))
+		{
+			AsyncOperation previewAsync = SceneManager.UnloadSceneAsync (preview);
+
+			while (!previewAsync.isDone) {
+				yield return  null;
+			}
+		}
+
+		AsyncOperation Async = SceneManager.LoadSceneAsync (loadSceneName, LoadSceneMode.Additive);
+		do {
+			yield return WaitForEndOfFrame ();
+		} 
+		while(!Async.isDone);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
