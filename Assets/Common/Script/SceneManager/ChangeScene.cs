99dd28cd8c2fe3aaff6af99acbadbf83b8359f0a
豊田智弘 <toyota@192.168.2.116>
2017-12-30 01:30:31 +0900

    ChangeScene

--- Assets/Common/Script/SceneManager/ChangeScene.cs
+++ Assets/Common/Script/SceneManager/ChangeScene.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ChangeScene : MonoBehaviour {
+public class ChangeScene : UnitySingleton <ChangeScene> {
 
 	private string preview;
 
