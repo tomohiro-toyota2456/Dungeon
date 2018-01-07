3eb3a4ec3b669f05e93b60ace0651e18ad056f2f
豊田智弘 <toyota@192.168.2.116>
2018-01-07 17:07:33 +0900

    エリアマップ作成

---
+++ Assets/SceneData/Area/Script/AreaManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AreaManager : MonoBehaviour {
+
+	[SerializeField]
+	Button[] areaButton;
+
+	// Use this for initialization
+	void Start () {
+		//ダンジョンデータの取得
+		DungeonDataBase.ShortData[] DungeonData = DataBaseManager.Instance.GetDataBase<DungeonDataBase>().GetShortDatas();
+		Button btn;
+		Text txt;
+		for (int i = 0; i<DungeonData.Length; i++) {
+			int index = i;
+			btn = areaButton [index];
+			txt = btn.gameObject.GetComponentInChildren<Text> ();
+			txt.text = DungeonData [index].name;
+			btn.onClick.AddListener(()=>OnClick(DungeonData[index].id));
+		}
+
+	}
+	public void OnClick(int id)
+	{
+		DungeonManager.DungeonId = id;
+		ChangeScene.Instance.LoadScene ("Game");
+	}
+		
+}
