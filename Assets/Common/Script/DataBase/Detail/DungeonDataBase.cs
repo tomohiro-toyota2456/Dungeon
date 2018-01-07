7976e0ed93158744954b367ef6c5d8657e1d84c6
harada2 <createdirect2@gmail.com>
2018-01-07 15:19:36 +0900

    ダミーデータ追加

--- Assets/Common/Script/DataBase/Detail/DungeonDataBase.cs
+++ Assets/Common/Script/DataBase/Detail/DungeonDataBase.cs
@@ -7,6 +7,12 @@ public class DungeonDataBase : DataBase
   [SerializeField]
   DungeonData[] dungeonDatas;
 
+  public struct ShortData
+  {
+    public int id;
+    public string name;
+  }
+
   public DungeonData Search(int id)
   {
     for(int i = 0; i < dungeonDatas.Length;i++)
@@ -18,5 +24,17 @@ public class DungeonDataBase : DataBase
     }
 
     return null;
+  }
+
+  public ShortData[] GetShortDatas()
+  {
+    ShortData[] shortDatas = new ShortData[4];//ダミー
+    for(int i = 0; i < shortDatas.Length; i++)
+    {
+      shortDatas[i].id = i;
+      shortDatas[i].name = "ダンジョン" + i.ToString();
+    }
+
+    return shortDatas;
   }
 }
