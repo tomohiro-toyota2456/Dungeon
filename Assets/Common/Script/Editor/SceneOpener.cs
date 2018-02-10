fe705dc1339e86eca15f18c160aa7c21199a6f47
harada2 <createdirect2@gmail.com>
2018-02-10 15:11:23 +0900

    シーンオープン機能追加

---
+++ Assets/Common/Script/Editor/SceneOpener.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public class SceneOpener : EditorWindow
+{
+  Vector2 scrollPos;
+
+  [MenuItem("SceneOpener/OpenWindow")]
+  static void Open()
+  {
+    GetWindow<SceneOpener>();
+  }
+
+  private void OnGUI()
+  {
+    GUILayout.Label("BuildSettingに登録されているシーンを開けます");
+    var scenes = EditorBuildSettings.scenes;
+
+    using (var scroll = new EditorGUILayout.ScrollViewScope(scrollPos))
+    {
+      for (int i = 0; i < scenes.Length; i++)
+      {
+        if (GUILayout.Button(scenes[i].path))
+        {
+
+          EditorSceneManager.OpenScene(scenes[i].path);
+        }
+      }
+
+      scrollPos = scroll.scrollPosition;
+    }
+  }
+}
